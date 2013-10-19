6fa7637fb4c473e67baae5ab21e2162860f4886a
Josh Lipps <josh.lipps@gmail.com>
2013-10-18 17:10:38 -0700

    added foundation support

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,5 @@
 exports.index = function(req, res){
 	var template_engine = req.app.settings.template_engine;
 	res.locals.session = req.session;
-  res.render('index', { title: 'TeamTakout'});
+  res.render('index', { title: 'Takout',page:'index'});
 };
